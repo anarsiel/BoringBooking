23a1b44dd67050f7ca0a9cbb943ae2bd6bc8a3c7
anarsiel <dimitrovblagoi@gmail.com>
2021-10-11 23:07:03 +0300

    configured heroku database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,15 +3,23 @@ import FluentPostgresDriver
 import Vapor
 
 public func configure(_ app: Application) throws {
-    app.databases.use(
-        .postgres(
-            hostname: "localhost",
-            username: "Vapor",
-            password: "Vapor",
-            database: "BoringBookingDB"
-        ),
-        as: .psql
-    )
+    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
+        postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
+        app.databases.use(.postgres(
+            configuration: postgresConfig
+        ), as: .psql)
+    }
+    else {
+        app.databases.use(
+            .postgres(
+                hostname: "localhost",
+                username: "Vapor",
+                password: "Vapor",
+                database: "BoringBookingDB"
+            ),
+            as: .psql
+        )
+    }
 
     app.migrations.add(CreateUsers())
     app.migrations.add(CreateRestaurants())
